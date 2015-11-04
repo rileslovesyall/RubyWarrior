0d5b50230c6c3628452e031e2a68624067bf4047
Riley Spicer <riley.r.spicer@gmail.com>
2015-11-03 16:44:10 -0800

    Lvl 5 added

---
+++ beginner/lvl5.rb
@@ -0,0 +1,24 @@
+class Player
+  def play_turn(warrior)
+    if @health == nil
+      @health = 20
+    end
+      if warrior.feel.empty?
+        if warrior.health < @health
+          warrior.walk!
+        else
+          if warrior.health < 15
+            warrior.rest!  
+          else
+            warrior.walk!
+          end
+        end
+      elsif warrior.feel.captive?
+        warrior.rescue!
+      else
+        warrior.attack!
+      end
+    @health = warrior.health
+  end
+end
+  
